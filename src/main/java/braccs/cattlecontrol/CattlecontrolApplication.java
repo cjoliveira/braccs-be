3e8d1a017e6d175f8134b2b61ca4306b5156d650
Carlos Oliveira <cjoliveira@outlook.com.br>
2024-01-25 19:25:20 -0300

    Alterando estrutura pacotes

---
+++ src/main/java/braccs/cattlecontrol/CattlecontrolApplication.java
@@ -0,0 +1,13 @@
+package braccs.cattlecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattlecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattlecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/braccs/cattlecontrol/CattlecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package braccs.cattlecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattlecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
